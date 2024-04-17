48b75dd0d59768f9f81bbbfee1296751b086e481
MikaelEngvall <mikael@engvall.org>
2024-04-17 22:12:57 +0200

    fixed the colors for light and dark mode

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,7 +18,7 @@ function Navbar() {
         const root = document.documentElement;
         root.style.setProperty('--bg-color-light', darkMode ? '#f2f2f2' : '#333333');
         root.style.setProperty('--text-color-light', darkMode ? '#333333' : '#f2f2f2');
-        root.style.setProperty('--bg-color-dark', darkMode ? '#333333' : '#f2f2f2');
+        root.style.setProperty('--bg-color-dark', darkMode ? '#cfc6c1' : '#777777');
         root.style.setProperty('--text-color-dark', darkMode ? '#f2f2f2' : '#333333');
     };
 
