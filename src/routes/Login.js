7cc08545b47abca8a79476398f2479749192fe3b
MikaelEngvall <mikael@engvall.org>
2024-04-19 00:35:35 +0200

    fixed login logic

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,18 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
-function Login() {
+function utf8_to_b64(str) {
+    return window.btoa(unescape(encodeURIComponent(str)));
+}
+
+function Login({ setIsAuthorized }) {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [loginError, setLoginError] = useState(false);
+
+    const navigate = useNavigate();
+
+    async function handleLogin(e) {
+        e.preventDefault();
+        const url = "http://localhost:8080/api/v1/users/login";
+
+        const basicAuth = utf8_to_b64(`${email}:${password}`);
+
+        try {
+            const response = await axios.post(
+                url,
+                {
+                    email: email,
+                    password: password,
+                },
+                {
+                    headers: {
+                        Authorization: `Basic ${basicAuth}`,
+                        "Content-Type": "application/json",
+                    },
+                }
+            );
+            console.log("User Log In Success");
+            handleAuthentication(response.data.data.token);
+            setIsAuthorized(true);
+        } catch (error) {
+            console.error("Error logging in", error);
+            setLoginError(true);
+        }
+    }
+
+    function handleAuthentication(token) {
+        localStorage.setItem("headhunter-token", token);
+        navigate("/account");
+    }
+
     return (
         <div className='login-container'>
             <div className='login-card'>
                 <h1>Login</h1>
-                <form>
+                <form onSubmit={handleLogin}>
                     <div className='form-group'>
                         <label htmlFor='email'>Email:</label>
-                        <input type='email' id='email' name='email' required />
+                        <input type='email' id='email' name='email' value={email} onChange={(e) => setEmail(e.target.value)} required />
                     </div>
                     <div className='form-group'>
                         <label htmlFor='password'>Password:</label>
-                        <input type='password' id='password' name='password' required />
+                        <input type='password' id='password' name='password' value={password} onChange={(e) => setPassword(e.target.value)} required />
                     </div>
                     <button type='submit'>Login</button>
                 </form>
