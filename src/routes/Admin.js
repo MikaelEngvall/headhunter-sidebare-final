9640d8876db04d162864f1c0154adb9d53706bef
MikaelEngvall <mikael@engvall.org>
2024-04-27 21:34:29 +0200

    Admin can delete and edit users now. email, username and/or roles

--- src/routes/Admin.js
+++ src/routes/Admin.js
@@ -5,6 +5,7 @@ function Admin() {
     const [users, setUsers] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
+    const [editedUser, setEditedUser] = useState(null);
     const url = "http://localhost:8080/api/v1/users/findAll";
 
     useEffect(() => {
@@ -40,7 +41,7 @@ function Admin() {
                     },
                 });
 
-                if (!response.ok) {
+                if (response.status !== 200) {
                     throw new Error('Failed to delete user');
                 }
 
@@ -49,6 +50,48 @@ function Admin() {
             } catch (error) {
                 console.error('Error deleting user:', error);
             }
+        }
+    };
+
+    const handleEdit = (index) => {
+        setEditedUser(users[index]);
+    };
+
+    const handleInputChange = (e, field) => {
+        const value = e.target.value;
+        setEditedUser(prevUser => ({
+            ...prevUser,
+            [field]: value
+        }));
+    };
+
+    const handleSave = async () => {
+        console.log("Edited user : ", editedUser.email);
+        try {
+            const response = await axios.put(`http://localhost:8080/api/v1/users/update/${editedUser.email}`, editedUser, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem("headhunter-token")}`,
+                    "Content-Type": "application/json",
+                },
+            });
+
+            if (response.status !== 200) {
+                throw new Error('Failed to update user');
+            }
+
+            // Update the user in the list
+            const updatedUsers = users.map(user => {
+                if (user.email === editedUser.email) {
+                    return editedUser;
+                }
+                return user;
+            });
+
+            setUsers(updatedUsers);
+            setEditedUser(null); // Clear edited user state
+            console.log('User updated successfully');
+        } catch (error) {
+            console.error('Error updating user:', error);
         }
     };
 
@@ -68,17 +111,47 @@ function Admin() {
                                     <th>ID</th>
                                     <th>Name</th>
                                     <th>Email</th>
+                                    <th>Roles</th>
                                     <th>Actions</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                {users.map((user) => (
+                                {users.map((user, index) => (
                                     <tr key={user?.id}>
                                         <td>{user?.id}</td>
-                                        <td>{user?.username}</td>
-                                        <td>{user?.email}</td>
                                         <td>
-                                            <button className='edit-button'>Edit</button> {/* Replace with custom edit functionality */}
+                                            {editedUser && editedUser.email === user.email ? (
+                                                <input
+                                                    type="text"
+                                                    value={editedUser.username}
+                                                    onChange={(e) => handleInputChange(e, 'username')}
+                                                />
+                                            ) : user.username}
+                                        </td>
+                                        <td>
+                                            {editedUser && editedUser.email === user.email ? (
+                                                <input
+                                                    type="email"
+                                                    value={editedUser.email}
+                                                    onChange={(e) => handleInputChange(e, 'email')}
+                                                />
+                                            ) : user.email}
+                                        </td>
+                                        <td>
+                                            {editedUser && editedUser.email === user.email ? (
+                                                <input
+                                                    type="text"
+                                                    value={editedUser.roles}
+                                                    onChange={(e) => handleInputChange(e, 'roles')}
+                                                />
+                                            ) : user.roles}
+                                        </td>
+                                        <td>
+                                            {editedUser && editedUser.email === user.email ? (
+                                                <button className='save-button' onClick={handleSave}>Save</button>
+                                            ) : (
+                                                <button className='edit-button' onClick={() => handleEdit(index)}>Edit</button>
+                                            )}
                                             <button className='delete-button' onClick={() => handleDelete(user.email)}>Delete</button>
                                         </td>
                                     </tr>
